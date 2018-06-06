2de344a15775ed91e53a73b6f413b9a429e31fcb
yacinty <yastaz1@alum.us.es>
2018-06-06 19:35:00 +0200

    Bootsrap y Jquery

--- src/main/webapp/views/master-page/layout.jsp
+++ src/main/webapp/views/master-page/layout.jsp
@@ -26,9 +26,12 @@
 
 	<link rel="shortcut icon" href="favicon.ico"/>
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<script src="jquery/dist/jquery.min.js"></script>
+	<link rel="stylesheet" href="bootstrap/dist/css/bootstrap.min.css">
+	<script type="text/javascript" src="bootstrap/dist/js/bootstrap.min.js"></script>
+
+
 
 
 	<link rel="stylesheet" href="styles/common.css" type="text/css">
--- src/test/java/services/FeedbackServiceTest.java
+++ src/test/java/services/FeedbackServiceTest.java
@@ -1,7 +1,9 @@
 package services;
 
 
+import domain.Feedback;
 import domain.Profile;
+import domain.Servise;
 import domain.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -41,7 +43,7 @@ public class FeedbackServiceTest extends AbstractTest {
                -. Leave a feedback on the services where he or she has participated.
     */
 
-    public void feedbackServiceTest(final String username, String userBean, final Class<?> expected) {
+    public void feedbackServiceTest( String username, String servisebean, String text,Integer points,  Class<?> expected) {
 
         Class<?> caught = null;
         startTransaction();
@@ -50,10 +52,19 @@ public class FeedbackServiceTest extends AbstractTest {
             this.authenticate(username);
 
             User result;
+            Servise servise = serviseService.findOne(getEntityId(servisebean));
 
-            result = userService.findOne(getEntityId(userBean));
+            Feedback feedback = feedbackService.create();
+            feedback.setServise(servise);
+            feedback.setText(text);
+            feedback.setPoints(points);
 
+            Feedback saved = feedbackService.save(feedback);
+            servise.getFeedbacks().add(feedback);
+            serviseService.save(servise);
 
+            feedbackService.flush();
+            serviseService.flush();
 
 
         } catch (final Throwable oops) {
@@ -74,22 +85,19 @@ public class FeedbackServiceTest extends AbstractTest {
 
         final Object testingData[][] = {
 
-                // Algun actor logueado con to do correcto-> true
+                // User deja un feedback en un servicio donde esta suscrito-> true
                 {
-                        "user3", "user3", "user3", "userTestName", "userTestSurname", "+34 123456789", "userTest@userTest.com", "addressTest", "fullNameTest", "http://www.images.com", "educationTest", null
+                        "user1", "servise4",  "text",4, null
                 },
-                // Patrón del telefono erroneo -> false
+                // User deja un feedback en un servicio donde No esta suscrito-> false
                 {
-                        "userTest3", "userTest3", "userTest3", "userTestName3", "userTestSurname3", "635", "managerTest@managerTest.com", "12345", "fullNameTest", "http://www.images.com", "educationTest", ConstraintViolationException.class
+                        "user1", "servise3",  "text",4, IllegalArgumentException.class
                 },
-                // Todos los campos como null --> false
-                {
-                        null, null, null, null, null, null, null, null, null, null, null, ConstraintViolationException.class
-                },
+
 
         };
         for (int i = 0; i < testingData.length; i++)
-            this.feedbackServiceTest((String) testingData[i][0], (String) testingData[i][1],(Class<?>) testingData[i][2]);
+            this.feedbackServiceTest((String) testingData[i][0], (String) testingData[i][1],(String) testingData[i][2], (Integer) testingData[i][3],(Class<?>) testingData[i][4]);
     }
 
 }
